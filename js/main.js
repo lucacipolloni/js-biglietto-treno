87da2d03a8a794f851e37e99d49cec69aee268ac
lucacipolloni <lucacipolloni29@gmail.com>
2025-04-02 16:58:58 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// # RACCOLTA DATI
+
+// * Inputs
+
+// Chiedo all'utente la sua età
+// Chiedo all'utente km da percorrere
+
+// * Costanti
+
+// Definisco il prezzo al km del biglietto
+// Definisco il prezzo base del biglietto
+// Definisco lo sconto % per minorenni
+// Definisco lo sconto % per over65
+
+// * Valori di output
+
+// Definisco il testo di output
+// Definisco il tipo di sconto applicato
+// Definisco amount di sconto applicato
+// Definisco il prezzo finale
+
+// # SVOLGIMENTO
+
+// Se l'utente è minorenne
+//   setto un tipo di sconto applicato (minorenni)
+//   setto un amount di sconto applicato (prezzo base * sconto % per over65 / 100)
+//   setto un prezzo finale (prezzo base - amount di sconto)
+
+// Se l'utente è over 65
+//   setto un tipo di sconto applicato (over65)
+//   setto un amount di sconto applicato (prezzo base * sconto % per over65 / 100)
+//   setto un prezzo finale (prezzo base - amount di sconto)
+
+// # OUTPUT
+
+// Se c'è tipo di sconto applicato
+//  Il messaggio è "Prezzo biglietto - tipo sconto applicato - amount e % - prezzo finale"
+// Altrimenti
+//  Comunico il prezzo finale
